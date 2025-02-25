fd5f893c3db5f3b80eb07aa8fa0d4939176b01cd
Hassan Wael <aqilanhassan@gmail.com>
2025-02-25 20:45:12 +0300

    make it MethodView

--- app.py
+++ app.py
@@ -1,65 +1,55 @@
 from flask import Flask, request
+from flask.views import MethodView
 from Student import Student
 
 students: list = []
-students.append(Student(0, 'John', 21, 90))
-students.append(Student(1, 'Jane', 22, 95))
 
 app = Flask(__name__)
 
+class StudentAPI(MethodView):
+    def get(self, student_id=None):
+        if student_id is None:
+            return {'students': [student.__dict__ for student in students]}
+        else:
+            try:
+                student = students[student_id]
+                return student.__dict__
+            except IndexError:
+                return {'response': f'{student_id} not found'}, 404
 
-@app.route('/hello/<name>')
-def home(name: str):
-    response = {'response': f'Hello, {name}, how is life?'}
-    return response
+    def post(self):
+        try:
+            id = students[-1].id + 1 if students else 0
+            name = request.json['name']
+            age = request.json['age']
+            grade = request.json['grade']
+            student = Student(id, name, age, grade)
+            students.append(student)
+            return {'response': f'{student.id} has been added to the list of students'}, 201
+        except KeyError as e:
+            return {'response': f'Missing parameter: {str(e)}'}, 400
 
+    def put(self, student_id):
+        try:
+            req = request.json
+            student = students[student_id]
+            student.update(req)
+            return student.__dict__
+        except (IndexError, KeyError) as e:
+            return {'response': f'Error updating student: {str(e)}'}, 400
 
-@app.route('/student', methods=['POST'])
-def create_student():
-    id = students[-1].id + 1
-    name = request.json['name']
-    age = request.json['age']
-    grade = request.json['grade']
-    student = Student(id, name, age, grade)
-    students[id] = student
-    return {'response': f'{student.id} has been added to the list of students'}
-
-
-@app.route('/students')
-def get_students():
-    return {'students': [student.__dict__ for student in students]}
-
-
-@app.route('/student/<int:id>')
-def get_student(id: int):
-    try:
-        student = students[id]
-        return student.__dict__
-    except IndexError:
-        return {'response': f'{id} not found'}
-
-
-@app.route('/student', methods=['PUT'])
-def update_student():
-
-    try:
-        req = request.json
-        id = req['id']
-        student = students[id]
-        student.update(req)
-        return student.__dict__
-    except Exception as e:
-        return {'response': e.__str__()}
+    def delete(self, student_id):
+        try:
+            student = students.pop(student_id)
+            return {'response': f'{student_id} has been removed from the list of students'}
+        except IndexError:
+            return {'response': f'{student_id} not found'}, 404
 
 
-@app.route('/student/<int:id>', methods=['DELETE'])
-def delete_student(id: int):
-    try:
-        students.remove(students[id])
-        return {'response': f'{id} has been removed from the list of students'}
-    except Exception as e:
-        return {'response': e.__str__()}
-
+# Register the class-based view with URL rules
+student_view = StudentAPI.as_view('student_api')
+app.add_url_rule('/student/', view_func=student_view, methods=['POST', 'GET'])
+app.add_url_rule('/student/<int:student_id>', view_func=student_view, methods=['GET', 'PUT', 'DELETE'])
 
 if __name__ == '__main__':
     app.run(debug=True)
